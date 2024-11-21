d8fa3c7db465b5e22c63430911962ef3a859a941
ProfRodolfoSJRP <163571628+ProfRodolfoSJRP@users.noreply.github.com>
2024-11-21 16:56:47 -0300

    Cadastro Usuário
    Criamos o sistema para cadastrar usuários no BD.

---
+++ cad.php
@@ -0,0 +1,38 @@
+<?php
+// Código para receber as informações do HTML e fazer algo
+// Captura o que o usuário digitou e cadastra no bd
+
+// Chama arquivo de conexão
+include 'conexao.php';
+
+// Verifica se existe alguma informação chegando pela Rede
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    // Recebe o e-mail, filtra e armazena na variavel
+    $email = htmlspecialchars($_POST['email']);
+
+    // Recebe a senha, criptografa e armazena em uma variavel
+    $senha = password_hash($_POST['senha'],PASSWORD_DEFAULT);
+
+    // Exibe a Variavel para testar 
+    //var_dump($senha);
+
+    // Bloco tente para cadastrar no banco de dados
+    try{
+        // Prepara o comando SQL para inserir no banco de dados
+        // Utilizar o Prepared para preverir injetar SQL 
+        $stmt = $conn->prepare("INSERT INTO Usuarios (email,senha) 
+                                VALUES (:email, :senha)");
+
+        // Associa os valores das variaveis :email e :senha 
+        $stmt->bindParam(":email",$email); // Vincula o e-mail e limpa 
+        $stmt->bindParam(":senha",$senha);
+
+        // Executa o código
+        $stmt->execute();
+
+        echo "Cadastrado com Sucesso";
+    }catch(PDOException $e){
+        echo "Erro ao cadastrar o usuário :".$e->getMessage();
+    }
+}
---
+++ cadastro.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <h2>Cadastro de Usuário</h2>
+        <form action="cad.php" method="post">
+            <p>Email : <input type="email" name="email"></p>
+            <p>Senha : <input type="senha" name="senha"></p>
+            <button type="submit">Cadastrar</button>
+        </form>
+    </body>
+</html>
