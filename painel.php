55518260a0f984161a1ccfb388a36be4c6a7bd5b
ProfRodolfoSJRP <163571628+ProfRodolfoSJRP@users.noreply.github.com>
2024-11-26 16:17:02 -0300

    Sistema de Logout

---
+++ painel.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tela Logada</title>
+    </head>
+    <body>
+        <h1>Bem Vindo à área Logada !</h1>
+        <p>Seu conteudo confidencial</p>
+        <a href="logout.php">Sair</a>
+    </body>
+</html>
